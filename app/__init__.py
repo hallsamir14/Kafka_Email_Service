deae598a2676ad95410eee2c18be7f4f05bfd6cf
samir <samir@hallserver.home>
2024-02-27 14:05:45 -0500

    Add plugin functionality. Adjust directives and dependencies.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,31 @@
+import pkgutil
+import importlib
 from app.commands import CommandHandler
-from app.commands.send import SendCommand
-from app.commands.exit import ExitCommand
+from app.commands import Command
+
 
 
 class App:
     def __init__(self):  # Constructor
         self.command_handler = CommandHandler()
 
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
+
     def start(self):
-        self.command_handler.register_command("send", SendCommand())
-        self.command_handler.register_command("exit", ExitCommand())
+        # Register commands here
+        self.load_plugins()
         print("Type 'exit' to exit.")
         while True:  #REPL Read, Evaluate, Print, Loop
             self.command_handler.execute_command(input(">>> ").strip())
