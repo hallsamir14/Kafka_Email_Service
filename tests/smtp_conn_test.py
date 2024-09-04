b57e0e7f3edcba6b48bf1ce1589b9eb419e5f9a7
jordanNeo <jordan.leebt@gmail.com>
2024-09-04 12:40:27 -0400

    useless os path code take out

--- tests/smtp_conn_test.py
+++ tests/smtp_conn_test.py
@@ -1,16 +1,5 @@
 import pytest
-import os
-import sys
 import smtplib
-
-# Get the directory of the current script (test file)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Get the parent directory (project root)
-project_root = os.path.dirname(current_dir)
-
-# Add the project root to the Python path
-sys.path.append(project_root)
 
 from app.utils.SMTP_connection import smtp_Settings
 
